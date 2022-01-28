7b119c3202d025aa2c1d44ef838f874325c901c5
Lorenzo <lolodi@microsoft.com>
2022-01-28 15:13:57 -0800

    Updating code for stats-api and stats-worker for new dependencies (#277)
    * stats-api is tested and working, the others have been updated but they haven't been tested yet or tested and not working
    
    * Update code to handle new breaking changes in stats-api and stats-worker

--- samples/todo-app/stats-api/server.js
+++ samples/todo-app/stats-api/server.js
@@ -1,36 +1,43 @@
 var express = require('express');
 var redis = require('redis');
-
 var app = express();
 
-var cache = redis.createClient({
-    host: process.env.STATS_CACHE_SERVICE_HOST,
-    port: process.env.STATS_CACHE_SERVICE_PORT,
-    tls: process.env.REDIS_SSL == "true" ? {
-        host: process.env.STATS_CACHE_SERVICE_HOST,
-        port: process.env.STATS_CACHE_SERVICE_PORT,
-    } : undefined,
+var cache = redis.createClient({    
+    socket:{
+        host:process.env.STATS_CACHE_SERVICE_HOST,
+        port:process.env.STATS_CACHE_SERVICE_PORT,
+        tls: process.env.REDIS_SSL == "true" ? {
+            host: process.env.STATS_CACHE_SERVICE_HOST,
+            port: process.env.STATS_CACHE_SERVICE_PORT,
+        } : undefined
+    },
     password: process.env.REDIS_PASSWORD || undefined
 });
+cache.on('error', (err) => console.log('Redis Client Error', err));
+cache.connect();
 
 app.get('/hello', function (req, res) {
     console.log('hello!');
     res.send('hello!');
 })
 
-app.get('/stats', function (req, res) {
-    console.log('request for stats received with kubernetes-route-as header: %s', req.get('kubernetes-route-as'));
-    cache.get('todosCreated', function (err, created) {
-        cache.get('todosCompleted', function (err, completed) {
-            cache.get('todosDeleted', function (err, deleted) {
-                res.send({
-                    todosCreated: created || 0,
-                    todosCompleted: completed || 0,
-                    todosDeleted: deleted || 0
-                });
+app.get('/stats', async function (req, res) {
+    console.log('request for stats received with kubernetes-route-as header: %s', req.get('kubernetes-route-as'));  
+    var created = 0;  
+    var completed = 0;
+    var deleted =0;
+    try{
+        var created = await cache.get('todosCreated');
+        var completed = await cache.get('todosCompleted');
+        var deleted = await cache.get('todosDeleted');
+    } catch(err) {
+        console.log(err);
+    }
+    res.send({
+                todosCreated: created || 0,
+                todosCompleted: completed || 0,
+                todosDeleted: deleted || 0
             });
-        });
-    });
 });
 
 var port = process.env.PORT || 3001;
@@ -42,9 +49,9 @@ process.on("SIGINT", () => {
     process.exit(130 /* 128 + SIGINT */);
 });
 
-process.on("SIGTERM", () => {
+process.on("SIGTERM", async () => {
     bus.close();
-    cache.quit();
+    await cache.disconnect();
     server.close(() => {
         process.exit(143 /* 128 + SIGTERM */);
     });
